c2ab29a241c6a90be3bc0f0ef44d4d2b72c3c98e
tfreebern2 <tfreebern2@gmail.com>
2018-09-14 14:07:18 -0400

    Refactoring to common crud interface. closes #11

--- pet-clinic-data/src/main/java/tim/freebern/sfgpetclinic/services/VetService.java
+++ pet-clinic-data/src/main/java/tim/freebern/sfgpetclinic/services/VetService.java
@@ -2,13 +2,6 @@ package tim.freebern.sfgpetclinic.services;
 
 import tim.freebern.sfgpetclinic.model.Vet;
 
-import java.util.Set;
+public interface VetService extends CrudService<Vet, Long> {
 
-public interface VetService {
-
-    Vet findById(Long id);
-
-    Vet save(Vet vet);
-
-    Set<Vet> findAll();
 }
